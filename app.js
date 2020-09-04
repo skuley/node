397e85a9ba01be50658d3de7b8116b4e8bf14d3d
skuley <skuleyandpotato@gmail.com>
2020-09-04 23:50:02 +0900

    learning response in node

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 // callback function
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello From my Node.js Server!</h1></body>');    
+    res.write('</html>');
+    // after writing headers and all the data to response body, we call end and it is point where must not write anymore.
+    res.end();
 });
 
 // listen funciton is a process where nodejs will not immediately exit our script but where
